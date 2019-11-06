726d801116b91daafe9a86edc18cd3a978b2af48
ckr3453 <david3453@naver.com>
2019-11-07 03:29:47 +0900

    modified 'settings.py' -  App 등록

--- AutoCArD_dev/settings.py
+++ AutoCArD_dev/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #앱 등록
+    'Dev_app.apps.DevAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +66,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'Dev_app.apps.DevAppConfig',
             ],
         },
     },
@@ -120,3 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Dev_app' ,'static')
+] #static 파일들이 어디에 있는지 쓰는곳
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# static 파일들이 어디로 모일 것인지 쓰는곳
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
