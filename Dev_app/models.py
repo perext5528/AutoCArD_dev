6e3c46db6bd4772e876e9afb6edef5b20061558f
perext5528 <perext5528@gmail.com>
2019-11-07 01:02:59 +0900

    Django app initial setting

---
+++ Dev_app/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class UserInfo(models.Model):
+    #Necessary
+    user_email = models.CharField(max_length=100)
+    user_pw = models.CharField(max_length=100)
+
+    #Optional
+    user_name = models.CharField(max_length=100)
+    uid = models.CharField(max_length=100)
+    user_history = models.IntegerField()
+
+
+#왜있는지 모름
+# class Post(models.Model):
+#     title = models.CharField(max_length=100)
+#     body = models.TextField()
+#
+# class BlogData(models.Model):
+#     title = models.CharField(max_length=200)
+#     link = models.URLField
